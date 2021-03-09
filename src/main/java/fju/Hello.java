a98c6e50ffbed3880e0cd9f85484b332e4f16efb
Alexia606707 <rebeccaho717@gmail.com>
2021-03-09 15:36:45 +0800

    object oriented programming started

---
+++ src/main/java/fju/Hello.java
@@ -0,0 +1,8 @@
+package fju;
+
+public class Hello {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.hello();
+    }
+}
--- src/main/java/fju/Person.java
+++ src/main/java/fju/Person.java
@@ -1,4 +1,7 @@
 package fju;
 
 public class Person {
+    public void hello() {
+        System.out.println("Hello World");
+    }
 }
