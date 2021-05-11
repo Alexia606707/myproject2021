6a0ca8ec06eb094e3883c48e1263d0ddf992aa2c
Alexia606707 <rebeccaho717@gmail.com>
2021-05-11 16:20:35 +0800

    better interface reporter

--- src/main/java/fju/a/Tester.java
+++ src/main/java/fju/a/Tester.java
@@ -2,9 +2,24 @@ package fju.a;
 
 public class Tester {
     public static void main(String[] args) {
-        Reporter reporter = new WeatherReporter();
-        Reporter reporter1 = new HeheheReporter();
+        Reporter reporter = new Reporter() {
+            @Override
+            public void print() {
+                System.out.println("Ding dong");
+            }
+        };
+        Reporter reporter1 = new Reporter() {
+            @Override
+            public void print() {
+                System.out.println("Hehehe");
+            }
+        };
         reporter.print();
         reporter1.print();
+
+//        Reporter reporter = new WeatherReporter();
+//        Reporter reporter1 = new HeheheReporter();
+//        reporter.print();
+//        reporter1.print();
     }
 }
