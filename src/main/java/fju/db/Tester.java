ed030c0e2a926f5173f766102e163728f902f380
Alexia606707 <rebeccaho717@gmail.com>
2021-06-22 16:37:13 +0800

    database

--- src/main/java/fju/db/Tester.java
+++ src/main/java/fju/db/Tester.java
@@ -1,9 +1,8 @@
 package fju.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tester {
     public static void main(String[] args) {
@@ -11,9 +10,20 @@ public class Tester {
             //Driver
             Class.forName("org.mariadb.jdbc.Driver");
             //Connection
-            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/demo?user=root&password=123456");
+            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/demo?user=root&password=rebeccacat0717");
             Statement statement = connection.createStatement();
-            statement.executeUpdate("insert into students values('123456', 'Hank'");
+//            statement.executeUpdate("insert into students values('00001', 'Hank')");
+            //
+            ResultSet rs = statement.executeQuery("select * from drinks");
+            List<Drink> drinks = new ArrayList<>();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+
+                System.out.println(id + "\t" + name + "\t" + price);
+                drinks.add(new Drink(id, name, price));
+            }
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
